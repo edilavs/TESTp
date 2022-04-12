a47a9e51b7e3818773e1dd3cfa8a0b0a6929f32e
validash <100703058+validash@users.noreply.github.com>
2022-04-12 11:49:27 +0400

    added new project

---
+++ PolymorphsimTHW/Journal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolyHW
+{
+    public class Journal : Product
+    {
+        public string Company { get; set; }
+        public override string GetInfo()
+        {
+            return Name + " " + Company;
+        }
+    }
+}
---
+++ PolymorphsimTHW/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PolyHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book();
+            book.Name = "Salam";
+            book.Price = 8.99;
+            book.Genre = "elmi";
+            book.Author = "hiko";
+
+            Book book1 = new Book();
+            book1.Name = "S11";
+            book1.Price = 21.99;
+            book1.Genre = "e78i";
+            book1.Author = "hikokokokok";
+
+            Library library = new Library();
+            library.AddProduct(book);
+            library.AddProduct(book1);
+            foreach (var item in library.GetProductsByPrice(20, 50))
+            {
+                item.GetInfo();
+            }
+        }
+    }
+}
